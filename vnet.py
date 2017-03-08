beb789cabc3b0f19e1aab9fc2e5b9487b4c2e3d5
Matt Macy <mmacy@nextbsd.org>
2017-03-08 11:29:43 -0800

    add missing arg and fix discrepancies

--- vnet.py
+++ vnet.py
@@ -18,14 +18,15 @@ import math
 class InputTransition(nn.Module):
     def __init__(self, outChannels):
         super(InputTransition, self).__init__()
-        self.conv1 = nn.Conv3D(, outChannels, kernel_size=5, padding=2)
+        self.conv1 = nn.Conv3D(1, outChannels, kernel_size=5, padding=2)
         
     def forward(self, x):
-        out = F.prelu(self.conv1(x))
+        # do we want a PRELU here as well?
+        out = self.conv1(x)
         # split input in to 16 channels
         x16 = torch.cat((x, x, x, x, x, x, x, x,
                          x, x, x, x, x, x, x, x), 1)
-        out = torch.add(out, x16)
+        out = F.prelu(torch.add(out, x16))
         return out
 
 class DownTransition(nn.Module):
@@ -45,19 +46,18 @@ class DownTransition(nn.Module):
         return out
         
 class UpTransition(nn.Module):
-    def __init__(self, inChannels, nConvs):
+    def __init__(self, inChans, outChans, nConvs):
         super(UpTransition, self).__init__()
-        outChannels = inChannels // 2
-        self.up_conv = nn.ConvTranspose3d(inChannels, outChannels, kernel_size=2, stride=2)
+        self.up_conv = nn.ConvTranspose3d(inChans, outChans // 2, kernel_size=2, stride=2)
         self.convs = []
         for _ in range(nConvs):
-            self.convs.append(nn.Conv3D(OutChannels, OutChannels, kernel_size=5, pad=2))
+            self.convs.append(nn.Conv3D(outChans, outChans, kernel_size=5, pad=2))
         
     def forward(self, x, skipx):
         out = F.prelu(self.up_conv(x))
+        out = xcat = torch.cat((out, skipx), 1)
         for conv in self.convs:
             out = F.prelu(conv(out))
-        xcat = torch.cat((x, skipx), 1)
         out = F.prelu(torch.add(out, xcat))
         return out
         
@@ -68,7 +68,7 @@ class OutputTransition(nn.Module):
         self.conv2 = nn.Conv3D(2, 2, kernel_size=1)
         
     def forward(self, x):
-        # convolve down to 2 channels
+        # convolve 32 down to 2 channels
         out = F.prelu(self.conv1(x))
         # do softmax of 1x1 convolution
         out = F.softmax(self.conv2(out))
@@ -85,11 +85,11 @@ class VNet(nn.Module):
         self.down_tr64 = DownTransition(32, 2)
         self.down_tr128 = DownTransition(64, 3)
         self.down_tr256 = DownTransition(128, 3)
-        self.up_tr256 = UpTransition(256, 3)
-        self.up_tr128 = UpTransition(128, 2)
-        self.up_tr64 = UpTransition(64, 1)
-        self.up_tr32 = UpTransition(32, 1)
-        self.out_tr = OutputTransition(16)
+        self.up_tr256 = UpTransition(256, 256, 3)
+        self.up_tr128 = UpTransition(256, 128, 2)
+        self.up_tr64 = UpTransition(128, 64, 1)
+        self.up_tr32 = UpTransition(64, 32, 1)
+        self.out_tr = OutputTransition(32)
 
     # The network topology as described in the diagram
     # in the VNet paper
