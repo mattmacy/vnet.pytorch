044a534e5686bbac3018f69bb4c69f6011cef974
Matt Macy <mmacy@nextbsd.org>
2017-03-14 23:46:04 -0700

    add dropout

--- vnet.py
+++ vnet.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+def passthrough(x, **kwargs):
+    return x
+
 class LUConv(nn.Module):
     def __init__(self, nchan, inplace):
         super(LUConv, self).__init__()
@@ -46,11 +49,12 @@ class InputTransition(nn.Module):
 
 
 class DownTransition(nn.Module):
-    def __init__(self, inChans, nConvs, inplace):
+    def __init__(self, inChans, nConvs, inplace, dropout=False):
         super(DownTransition, self).__init__()
         outChans = 2*inChans
         self.down_conv = nn.Conv3d(inChans, outChans, kernel_size=2, stride=2)
         self.bn1 = nn.BatchNorm3d(outChans)
+        self.do1 = passthrough
 
         if inplace:
             self.relu1 = nn.ReLU(inplace=inplace)
@@ -58,30 +62,37 @@ class DownTransition(nn.Module):
         else:
             self.relu1 = nn.PReLU()
             self.relu2 = nn.PReLU()
+        if dropout:
+            self.do1 = nn.Dropout3d(p=0.2)
         self.ops = _make_nConv(outChans, nConvs, inplace)
 
     def forward(self, x):
         down = self.relu1(self.bn1(self.down_conv(x)))
-        out = self.ops(down)
+        out = self.do1(down)
+        out = self.ops(out)
         out = self.relu2(torch.add(out, down))
         return out
 
 
 class UpTransition(nn.Module):
-    def __init__(self, inChans, outChans, nConvs, inplace):
+    def __init__(self, inChans, outChans, nConvs, inplace, dropout=False):
         super(UpTransition, self).__init__()
         self.up_conv = nn.ConvTranspose3d(inChans, outChans // 2, kernel_size=2, stride=2)
         self.bn1 = nn.BatchNorm3d(outChans // 2)
+        self.do1 = passthrough
         if inplace:
             self.relu1 = nn.ReLU(inplace=inplace)
             self.relu2 = nn.ReLU(inplace=inplace)
         else:
             self.relu1 = nn.PReLU()
             self.relu2 = nn.PReLU()
+        if dropout:
+            self.do1 = nn.Dropout3d(p=0.2)
         self.ops = _make_nConv(outChans, nConvs, inplace)
 
     def forward(self, x, skipx):
-        out = self.relu1(self.bn1(self.up_conv(x)))
+        out = self.do1(x)
+        out = self.relu1(self.bn1(self.up_conv(out)))
         xcat = torch.cat((out, skipx), 1)
         out = self.ops(xcat)
         out = self.relu2(torch.add(out, xcat))
@@ -127,10 +138,10 @@ class VNet(nn.Module):
         self.in_tr = InputTransition(16, inplace)
         self.down_tr32 = DownTransition(16, 1, inplace)
         self.down_tr64 = DownTransition(32, 2, inplace)
-        self.down_tr128 = DownTransition(64, 3, inplace)
-        self.down_tr256 = DownTransition(128, 3, inplace)
-        self.up_tr256 = UpTransition(256, 256, 3, inplace)
-        self.up_tr128 = UpTransition(256, 128, 2, inplace)
+        self.down_tr128 = DownTransition(64, 3, inplace, dropout=True)
+        self.down_tr256 = DownTransition(128, 3, inplace, dropout=True)
+        self.up_tr256 = UpTransition(256, 256, 3, inplace, dropout=True)
+        self.up_tr128 = UpTransition(256, 128, 2, inplace, dropout=True)
         self.up_tr64 = UpTransition(128, 64, 1, inplace)
         self.up_tr32 = UpTransition(64, 32, 1, inplace)
         self.out_tr = OutputTransition(32, batchSize, inplace, nll)
