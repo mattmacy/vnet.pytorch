2d31fd3045d20f31bc6a31c3ebb7af6d04d3a2b4
Matt Macy <mmacy@nextbsd.org>
2017-03-16 00:33:09 -0700

    add dropout to skip connections and re-name inplace to elu

--- vnet.py
+++ vnet.py
@@ -7,12 +7,12 @@ def passthrough(x, **kwargs):
     return x
 
 class LUConv(nn.Module):
-    def __init__(self, nchan, inplace):
+    def __init__(self, nchan, elu):
         super(LUConv, self).__init__()
-        if inplace:
-            self.prelu = nn.ReLU(inplace=inplace)
+        if elu:
+            self.prelu = nn.ELU(inplace=True)
         else:
-            self.prelu = nn.PReLU()
+            self.prelu = nn.PReLU(nchan)
         self.conv = nn.Conv3d(nchan, nchan, kernel_size=5, padding=2)
         self.bn1 = nn.BatchNorm3d(nchan)
 
@@ -21,22 +21,22 @@ class LUConv(nn.Module):
         return out
 
 
-def _make_nConv(nchan, depth, inplace):
+def _make_nConv(nchan, depth, elu):
     layers = []
     for _ in range(depth):
-        layers.append(LUConv(nchan, inplace))
+        layers.append(LUConv(nchan, elu))
     return nn.Sequential(*layers)
 
 
 class InputTransition(nn.Module):
-    def __init__(self, outChans, inplace):
+    def __init__(self, outChans, elu):
         super(InputTransition, self).__init__()
         self.conv1 = nn.Conv3d(1, 16, kernel_size=5, padding=2)
         self.bn1 = nn.BatchNorm3d(16)
-        if inplace:
-            self.relu1 = nn.ReLU(inplace=inplace)
+        if elu:
+            self.relu1 = nn.ELU(inplace=True)
         else:
-            self.relu1 = nn.PReLU()
+            self.relu1 = nn.PReLU(16)
 
     def forward(self, x):
         # do we want a PRELU here as well?
@@ -49,22 +49,22 @@ class InputTransition(nn.Module):
 
 
 class DownTransition(nn.Module):
-    def __init__(self, inChans, nConvs, inplace, dropout=False):
+    def __init__(self, inChans, nConvs, elu, dropout=False):
         super(DownTransition, self).__init__()
         outChans = 2*inChans
         self.down_conv = nn.Conv3d(inChans, outChans, kernel_size=2, stride=2)
         self.bn1 = nn.BatchNorm3d(outChans)
         self.do1 = passthrough
 
-        if inplace:
-            self.relu1 = nn.ReLU(inplace=inplace)
-            self.relu2 = nn.ReLU(inplace=inplace)
+        if elu:
+            self.relu1 = nn.ELU(inplace=True)
+            self.relu2 = nn.ELU(inplace=True)
         else:
-            self.relu1 = nn.PReLU()
-            self.relu2 = nn.PReLU()
+            self.relu1 = nn.PReLU(outChans)
+            self.relu2 = nn.PReLU(outChans)
         if dropout:
-            self.do1 = nn.Dropout3d(p=0.2)
-        self.ops = _make_nConv(outChans, nConvs, inplace)
+             self.do1 = nn.Dropout3d()
+        self.ops = _make_nConv(outChans, nConvs, elu)
 
     def forward(self, x):
         down = self.relu1(self.bn1(self.down_conv(x)))
@@ -75,41 +75,42 @@ class DownTransition(nn.Module):
 
 
 class UpTransition(nn.Module):
-    def __init__(self, inChans, outChans, nConvs, inplace, dropout=False):
+    def __init__(self, inChans, outChans, nConvs, elu, dropout=False):
         super(UpTransition, self).__init__()
         self.up_conv = nn.ConvTranspose3d(inChans, outChans // 2, kernel_size=2, stride=2)
         self.bn1 = nn.BatchNorm3d(outChans // 2)
         self.do1 = passthrough
-        if inplace:
-            self.relu1 = nn.ReLU(inplace=inplace)
-            self.relu2 = nn.ReLU(inplace=inplace)
+        self.do2 = nn.Dropout3d()
+        if elu:
+            self.relu1 = nn.ELU(inplace=True)
+            self.relu2 = nn.ELU(inplace=True)
         else:
-            self.relu1 = nn.PReLU()
-            self.relu2 = nn.PReLU()
+            self.relu1 = nn.PReLU(outChans // 2)
+            self.relu2 = nn.PReLU(outChans)
         if dropout:
-            self.do1 = nn.Dropout3d(p=0.2)
-        self.ops = _make_nConv(outChans, nConvs, inplace)
+            self.do1 = nn.Dropout3d()
+        self.ops = _make_nConv(outChans, nConvs, elu)
 
     def forward(self, x, skipx):
         out = self.do1(x)
+        skipxdo = self.do2(skipx)
         out = self.relu1(self.bn1(self.up_conv(out)))
-        xcat = torch.cat((out, skipx), 1)
+        xcat = torch.cat((out, skipxdo), 1)
         out = self.ops(xcat)
         out = self.relu2(torch.add(out, xcat))
         return out
 
 
 class OutputTransition(nn.Module):
-    def __init__(self, inChans, batchSize, inplace, nll):
+    def __init__(self, inChans, elu, nll):
         super(OutputTransition, self).__init__()
         self.conv1 = nn.Conv3d(inChans, 2, kernel_size=5, padding=2)
         self.bn1 = nn.BatchNorm3d(2)
         self.conv2 = nn.Conv3d(2, 2, kernel_size=1)
-        self.batchSize = batchSize
-        if inplace:
-            self.relu1 = nn.ReLU(inplace=inplace)
+        if elu:
+            self.relu1 = nn.ELU(inplace=True)
         else:
-            self.relu1 = nn.PReLU()
+            self.relu1 = nn.PReLU(2)
         if nll:
             self.softmax = F.log_softmax
         else:
@@ -133,18 +134,19 @@ class OutputTransition(nn.Module):
 class VNet(nn.Module):
     # the number of convolutions in each layer corresponds
     # to what is in the actual prototxt, not the intent
-    def __init__(self, batchSize, inplace=True, nll=False):
+    def __init__(self, elu=True, nll=False):
         super(VNet, self).__init__()
-        self.in_tr = InputTransition(16, inplace)
-        self.down_tr32 = DownTransition(16, 1, inplace)
-        self.down_tr64 = DownTransition(32, 2, inplace)
-        self.down_tr128 = DownTransition(64, 3, inplace, dropout=True)
-        self.down_tr256 = DownTransition(128, 3, inplace, dropout=True)
-        self.up_tr256 = UpTransition(256, 256, 3, inplace, dropout=True)
-        self.up_tr128 = UpTransition(256, 128, 2, inplace, dropout=True)
-        self.up_tr64 = UpTransition(128, 64, 1, inplace)
-        self.up_tr32 = UpTransition(64, 32, 1, inplace)
-        self.out_tr = OutputTransition(32, batchSize, inplace, nll)
+        self.in_tr = InputTransition(16, elu)
+        self.down_tr32 = DownTransition(16, 1, elu)
+        self.down_tr64 = DownTransition(32, 2, elu)
+        self.down_tr128 = DownTransition(64, 3, elu, dropout=True)
+        self.down_tr256 = DownTransition(128, 3, elu, dropout=True)
+        self.up_tr256 = UpTransition(256, 256, 3, elu, dropout=True)
+        self.up_tr128 = UpTransition(256, 128, 2, elu, dropout=True)
+        self.up_tr64 = UpTransition(128, 64, 1, elu)
+        self.up_tr32 = UpTransition(64, 32, 1, elu)
+        self.out_tr = OutputTransition(32, elu, nll)
+
 
 
     # The network topology as described in the diagram
